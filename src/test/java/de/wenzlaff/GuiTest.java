faea14db898f34139b727e47f9a85f4acdde8c24
Thomas Wenzlaff <karotz@wenzlaff.de>
2016-12-15 17:57:01 +0100

    JUnit Test erg.

--- src/test/java/de/wenzlaff/GuiTest.java
+++ src/test/java/de/wenzlaff/GuiTest.java
@@ -1,14 +1,12 @@
 package de.wenzlaff;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class GuiTest {
 
 	@Test
 	public void testGui() {
-		assertTrue(true);
+		Gui.main(null);
 	}
 
 }
